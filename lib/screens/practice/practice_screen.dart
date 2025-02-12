dd6e3600575a7bf9dca64f7acd5ac8a98e3b0d59
Nikko Laurenciana <nblaurenciana@gmail.com>
2025-02-12 16:37:47 +0100

    Add const constructors to HomeScreen, PracticeScreen, and SettingsScreen

--- lib/screens/practice/practice_screen.dart
+++ lib/screens/practice/practice_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class PracticeScreen extends StatelessWidget {
+  const PracticeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
