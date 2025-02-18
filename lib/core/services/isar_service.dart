337a923d92f9de9497c47a628d55b9bd83fdb156
Nikko Laurenciana <nblaurenciana@gmail.com>
2025-02-18 16:39:47 +0100

    Implement IsarService for database management; add methods for initializing, adding, retrieving, and deleting items

---
+++ lib/core/services/isar_service.dart
@@ -0,0 +1,38 @@
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:wortmeister/data/models/deck.dart';
+import 'package:wortmeister/data/models/word.dart';
+
+class IsarService {
+  late Future<Isar> db;
+
+  IsarService() {
+    db = _initDb();
+  }
+
+  Future<Isar> _initDb() async {
+    final dir = await getApplicationDocumentsDirectory();
+    if (!await dir.exists()) {
+      await dir.create(recursive: true);
+    }
+    return await Isar.open(
+      [WordSchema, DeckSchema, WordSchema],
+      directory: dir.path,
+    );
+  }
+
+  Future<void> addItem<T>(T item) async {
+    final isar = await db;
+    isar.writeTxnSync(() => isar.collection<T>().putSync(item));
+  }
+
+  Future<List<T>> getItems<T>() async {
+    final isar = await db;
+    return await isar.collection<T>().where().findAll();
+  }
+
+  Future<void> deleteItem<T>(int id) async {
+    final isar = await db;
+    isar.writeTxnSync(() => isar.collection<T>().deleteSync(id));
+  }
+}
